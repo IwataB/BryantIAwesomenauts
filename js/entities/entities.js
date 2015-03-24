18e86154dbeda8f3dc2853bb2d05d2f07e2f4b57
Bryant Iwata <pbomber07@gmail.com>
2015-03-24 09:13:08 -0700

    End of 3/24/15

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,9 +13,12 @@ game.PlayerEntity = me.Entity.extend({
             }]);
 
         this.body.setVelocity(5, 20);
+        this.facing = "right";
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH)
 
         this.renderable.addAnimation("idle", [78]);
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
+        this.renderable.addAnimation("attack", [65, 66, 67, 68, 69, 70, 71, 72], 80);
 
         this.renderable.setCurrentAnimation("idle");
     },
@@ -25,102 +28,143 @@ game.PlayerEntity = me.Entity.extend({
             //setVelocity() and multiplying it by me.timer.tick.
             //me.timer.tick
             this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.facing = "right"
             this.flipX(true);
         }
-        else {
+        else if (me.input.isKeyPressed("left")) {
+            this.facing = "left";
+            this.body.vel.x -= this.body.accel.x * me.timer.tick;
+            this.flipX(false);
+        } else {
             this.body.vel.x = 0;
         }
-        if (this.body.vel.x !== 0) {
+
+        if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling){
+            this.jumping = true;
+            this.body.vel.y -= this.body.accel.y * me.timer.tick;
+        }
+
+        if (me.input.isKeyPressed("attack")) {
+            if (!this.renderable.isCurrentAnimation("attack")) {
+                console.log("hi");
+                //sets the current animation to the attack animation
+                //after the attack animation finishes it sets back to idle
+                this.renderable.setCurrentAnimation("attack", "idle");
+                //makes it so that the next time we start the animation
+                //it starts from the first frame not where we left off
+                this.renderable.setAnimationFrame();
+            }
+        }
+
+        else if (this.body.vel.x !== 0) {
 
             if (!this.renderable.isCurrentAnimation("walk")) {
                 this.renderable.setCurrentAnimation("walk");
             }
-        }else{
+        } else {
             this.renderable.setCurrentAnimation("idle");
         }
-
+        me.collision.check(this, true, this.collideHandler.bind(this), true);
         this.body.update(delta);
 
         this._super(me.Entity, "update", [delta]);
         return true;
+    },
+    
+    collideHandler: function(response){
+        if(response.b.type === 'enemyBaseEntity'){
+            var ydif = this.pos.y - response.b.pos.y;
+            var xdif = this.pos.x - response.b.pos.x;
+            
+            if(ydif<-40 && xdif>70 && xdif<-35){
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }
+            else if(xdif<-35 && this.facing === 'right' && (xdif<0)){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x -1;
+            }   
+            else if(xdif<60 && this.facing === 'left' && xdif>0){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x +1;
+            }
+        }
     }
 });
 
 game.PlayerBaseEntity = me.Entity.extend({
-    init : function (x, y, settings){
+    init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, {
-          image: "tower",
-          width: 100,
-          height: 100,
-          sritewidth: "100",
-          spriteheight: "100",
-          getShape: function(){
-              return (new me.Rect(0, 0, 100, 70)).toPolygon();
-          }
-        }]);
+                image: "tower",
+                width: 100,
+                height: 100,
+                sritewidth: "100",
+                spriteheight: "100",
+                getShape: function() {
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
+                }
+            }]);
         this.broken = false;
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        
+
         this.type = "PlayerBaseEntity";
-        
+
         this.renderable.addAnimation("idle", [0]);
         this.renderable.addAnimation("broken", [1]);
         this.renderable.setCurrentAnimation("idle");
     },
-    
-    update:function(delta){
-        if(this.health<=0){
+    update: function(delta) {
+        if (this.health <= 0) {
             this.broken = true;
             this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
-        
+
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    onCollision: function(){
-        
-        
+    onCollision: function() {
+
+
     }
 });
 
 game.EnemyBaseEntity = me.Entity.extend({
-    init : function (x, y, settings){
+    init: function(x, y, settings) {
         this._super(me.Entity, 'init', [x, y, {
-          image: "tower",
-          width: 100,
-          height: 100,
-          sritewidth: "100",
-          spriteheight: "100",
-          getShape: function(){
-              return (new me.Rect(0, 0, 100, 70)).toPolygon();
-          }
-        }]);
+                image: "tower",
+                width: 100,
+                height: 100,
+                sritewidth: "100",
+                spriteheight: "100",
+                getShape: function() {
+                    return (new me.Rect(0, 0, 100, 70)).toPolygon();
+                }
+            }]);
         this.broken = false;
         this.health = 10;
         this.alwaysUpdate = true;
         this.body.onCollision = this.onCollision.bind(this);
-        
+
         this.type = "EnemyBaseEntity";
-        
+
         this.renderable.addAnimation("idle", [0]);
         this.renderable.addAnimation("broken", [1]);
         this.renderable.setCurrentAnimation("idle");
     },
-    
-    update:function(delta){
-        if(this.health<=0){
+    update: function(delta) {
+        if (this.health <= 0) {
             this.broken = true;
             this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
-        
+
         this._super(me.Entity, "update", [delta]);
         return true;
     },
-    onCollision: function(){
-           
+    onCollision: function() {
+
     }
 });
